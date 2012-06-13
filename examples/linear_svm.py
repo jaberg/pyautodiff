472a5697aa147cf8ba7ab8a552bbb807ec93f2cf
James Bergstra <james.bergstra@gmail.com>
2012-06-13 11:58:15 -0400

    simplified and documented example

--- examples/linear_svm.py
+++ examples/linear_svm.py
@@ -2,45 +2,55 @@
 Linear SVM
 ==========
 
-This example illustrates how a function defined purely by numpy operations can
-be minimized directly with a gradient-based solver.
+This script fits a linear support vector machine classifier to random data.  It
+illustrates how a function defined purely by numpy operations can be minimized
+directly with a gradient-based solver.
 
-This example is a linear classifier (support vector machine) applied to random
-data.
+Program output:
+
+```
+ran loss_fn(), returning 1.0
+ran loss_fn(), returning 0.722904977725
+Best-fit SVM:
+ -> cost: 0.722904977725
+ -> weights: [-0.61920868 -0.68296249  0.90574115  1.15135323 -0.69036838]
+ -> bias: -0.0152805125301
+```
 
 """
 
-import sys
 import numpy as np
 from autodiff import fmin_l_bfgs_b
+from functools import partial
 
-def binary_svm_hinge_loss(weights, bias, x, y, l2_regularization):
-    # print x, y
+np.random.seed(1)
+
+# -- create some fake data
+x = np.random.rand(10, 5)
+y = 2 * (np.random.rand(10) > 0.5) - 1
+l2_regularization = 1e-4
+
+def loss_fn(weights, bias):
     margin = y * (np.dot(x, weights) + bias)
     loss = np.maximum(0, 1 - margin) ** 2
     l2_cost = 0.5 * l2_regularization * np.dot(weights, weights)
     loss = np.mean(loss) + l2_cost
+    print 'ran loss_fn(), returning', loss
     return loss
 
+# -- Run loss_fn once to trace computations.
+w, b = fmin_l_bfgs_b(loss_fn, [np.zeros(5), np.zeros(())])
 
-def main():
+# What happened here?
+# The computation is repeated many times during the optimization process by
+# bytecode *derived* from loss_fn, in which some things (e.g. the print
+# statement) have been removed.
 
-    # create some fake data
-    x = np.random.rand(10, 7)
-    y = 2 * (np.random.rand(10) > 0.5) - 1
+# -- run loss_fn as usual
+final_loss = loss_fn(w, b)
 
-    def loss_fn(w, b):
-        return binary_svm_hinge_loss(w, b, x, y, 1e-4)
-
-    w, b = fmin_l_bfgs_b(loss_fn, [np.zeros(7), np.zeros(())])
-
-    print 'Best-fit SVM:'
-    print ' -> cost:', loss_fn(w, b)
-    print ' -> weights:', w
-    print ' -> bias:', b
-
-
-if __name__ == '__main__':
-    np.random.seed(1)
-    sys.exit(main())
+print 'Best-fit SVM:'
+print ' -> cost:', final_loss
+print ' -> weights:', w
+print ' -> bias:', b
 
