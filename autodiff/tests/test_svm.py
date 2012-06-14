758207114b1d43bb93913668def5c5e9c30e37d2
James Bergstra <james.bergstra@gmail.com>
2012-06-14 15:01:07 -0400

    svm test case

---
+++ autodiff/tests/test_svm.py
@@ -0,0 +1,24 @@
+import numpy as np
+from autodiff import fmin_l_bfgs_b
+
+def test_svm():
+
+    rng = np.random.RandomState(1)
+
+    # -- create some fake data
+    x = rng.rand(10, 5)
+    y = 2 * (rng.rand(10) > 0.5) - 1
+    l2_regularization = 1e-4
+
+    def loss_fn(weights, bias):
+        margin = y * (np.dot(x, weights) + bias)
+        loss = np.maximum(0, 1 - margin) ** 2
+        l2_cost = 0.5 * l2_regularization * np.dot(weights, weights)
+        loss = np.mean(loss) + l2_cost
+        print 'ran loss_fn(), returning', loss
+        return loss
+
+    w, b = fmin_l_bfgs_b(loss_fn, [np.zeros(5), np.zeros(())])
+    final_loss = loss_fn(w, b)
+    assert np.allclose(final_loss, 0.7229)
+
