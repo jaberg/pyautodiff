5a98751ba5f7ac0284a1f58a4353507bc6d10ecf
James Bergstra <james.bergstra@gmail.com>
2012-06-20 15:50:03 -0400

    fmin_l_bfgs_b refactor

--- autodiff/fmin_scipy.py
+++ autodiff/fmin_scipy.py
@@ -3,6 +3,7 @@ Scipy-based function minimization drivers
 
 """
 
+import time
 import numpy as np
 import scipy.optimize.lbfgsb
 import theano
@@ -10,50 +11,51 @@ import theano
 from .context import Context
 
 
-def fmin_l_bfgs_b(fn, args, **scipy_kwargs):
-    """
-    Return values that minimize Python function `fn(*args)`, by automatically
-    differentiating `fn` and applying scipy's fmin_l_bfgs_b routine.
+def vector_from_args(args):
+    args_sizes = [w.size for w in args]
+    x_size = sum(args_sizes)
+    x = np.empty(x_size, dtype='float64') # has to be float64 for fmin_l_bfgs_b
+    return x
 
-    Parameters
-    ----------
-    fn: a scalar-valued function of floats and float/complex arguments
 
-    args: a list of floats / complex / ndarrays from from which to start
-        optimizing `fn(*args)`
-        TODO: support a single float, ndarray, and also tuples, dicts, etc.
+def args_from_vector(x, orig_args):
+    # unpack x_opt -> args-like structure `args_opt`
+    rval = []
+    i = 0
+    for w in orig_args:
+        rval.append(x[i: i + w.size].reshape(w.shape).astype(w.dtype))
+        i += w.size
+    return rval
 
-    **scipy_kwargs: pass these through to scipy's fmin_l_bfgs_b routine
 
-    """
-    # XXX remove algo param, make each algo a separate fmin function
-
-    # STEP 1: inspect bytecode of fn to determine derivative wrt args
+def theano_f_df(fn, args, mode):
+    # -- inspect bytecode of fn to determine derivative wrt args
 
     # hacky way to get call graph (we could do it without actually running it)
     ctxt = Context()
     cost = ctxt.call(fn, args)
-
 
     # construct bytecode for f_df() that
     # * unpacks x-> args
     # * computes f, dx
     orig_s_args = [ctxt.svars[id(w)] for w in args]
     args_shapes = [w.shape for w in args]
-    args_sizes = [w.size for w in args]
-    x_size = sum(args_sizes)
-    x = np.empty(x_size, dtype='float64') # has to be float64 for fmin_l_bfgs_b
+    x = vector_from_args(args)
     s_x = theano.tensor.vector(dtype=x.dtype)
     s_args = []
     i = 0
-    for w in args:
+    for s_w, w in zip(orig_s_args, args):
         x[i: i + w.size] = w.flatten()
         if w.shape:
             s_xi = s_x[i: i + w.size].reshape(w.shape)
         else:
             s_xi = s_x[i]
+        s_arg_i = theano.tensor.patternbroadcast(
+                s_xi.astype(str(w.dtype)),
+                broadcastable=s_w.broadcastable)
+        s_args.append(s_arg_i)
         i += w.size
-        s_args.append(s_xi.astype(str(w.dtype)))
+
 
     orig_s_cost = ctxt.svars[id(cost)]
     memo = theano.gof.graph.clone_get_equiv(
@@ -70,19 +72,36 @@ def fmin_l_bfgs_b(fn, args, **scipy_kwargs):
     # XXX: hacky current thing does not pass by a proper byte-code optimizer
     # because numba isn't quite there yet. For now we just compile the call
     # graph we already built theano-style.
-    f_df = theano.function([s_x], [s_cost, g_x])
+    f_df = theano.function([s_x], [s_cost, g_x], mode=mode)
+    return f_df
 
+
+def fmin_l_bfgs_b(fn, args, theano_mode=None, **scipy_kwargs):
+    """
+    Return values that minimize Python function `fn(*args)`, by automatically
+    differentiating `fn` and applying scipy's fmin_l_bfgs_b routine.
+
+    Parameters
+    ----------
+    fn: a scalar-valued function of floats and float/complex arguments
+
+    args: a list of floats / complex / ndarrays from from which to start
+        optimizing `fn(*args)`
+        TODO: support a single float, ndarray, and also tuples, dicts, etc.
+
+    **scipy_kwargs: pass these through to scipy's fmin_l_bfgs_b routine
+
+    """
+
+    f_df = theano_f_df(fn, args, mode=theano_mode)
+
+    x = vector_from_args(args)
     # pass control to iterative minimizer
     #x_opt, mincost, info_dct = fmin_l_bfgs_b(f_df, x, **fmin_kwargs)
     x_opt, mincost, info_dct = scipy.optimize.lbfgsb.fmin_l_bfgs_b(
             f_df, x, **scipy_kwargs)
 
-    # unpack x_opt -> args-like structure `args_opt`
-    rval = []
-    i = 0
-    for w in args:
-        rval.append(x_opt[i: i + w.size].reshape(w.shape))
-        i += w.size
+    rval = args_from_vector(x_opt, args)
     # XXX: one of the scipy_kwargs says to return more/less info,
     #     and that should be reflected here too.
     return rval #, mincost, info_dct
