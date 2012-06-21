ab33cc048793a17278188894f5762600c208b68a
James Bergstra <james.bergstra@gmail.com>
2012-06-21 18:48:29 -0400

    ENH: fmin_l_bfgs_b supports scalar_bounds arg

--- autodiff/fmin_scipy.py
+++ autodiff/fmin_scipy.py
@@ -72,11 +72,13 @@ def theano_f_df(fn, args, mode):
     # XXX: hacky current thing does not pass by a proper byte-code optimizer
     # because numba isn't quite there yet. For now we just compile the call
     # graph we already built theano-style.
+    #s_cost = theano.printing.Print('s_cost')(s_cost)
     f_df = theano.function([s_x], [s_cost, g_x], mode=mode)
     return f_df
 
 
-def fmin_l_bfgs_b(fn, args, theano_mode=None, **scipy_kwargs):
+def fmin_l_bfgs_b(fn, args, theano_mode=None, scalar_bounds=None,
+        **scipy_kwargs):
     """
     Return values that minimize Python function `fn(*args)`, by automatically
     differentiating `fn` and applying scipy's fmin_l_bfgs_b routine.
@@ -96,6 +98,15 @@ def fmin_l_bfgs_b(fn, args, theano_mode=None, **scipy_kwargs):
     f_df = theano_f_df(fn, args, mode=theano_mode)
 
     x = vector_from_args(args)
+
+    if scalar_bounds is not None:
+        lb, ub = scalar_bounds
+        bounds = np.empty((len(x), 2))
+        bounds[:, 0] = lb
+        bounds[:, 1] = ub
+        if 'bounds' in scipy_kwargs:
+            raise TypeError('duplicate argument: bounds')
+        scipy_kwargs['bounds'] = bounds
     # pass control to iterative minimizer
     #x_opt, mincost, info_dct = fmin_l_bfgs_b(f_df, x, **fmin_kwargs)
     x_opt, mincost, info_dct = scipy.optimize.lbfgsb.fmin_l_bfgs_b(
